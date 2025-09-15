d0610623d4ccc6eb40f286299082d8da20b17006
wonjun199908 <wonjun199908@gmail.com>
2025-09-15 10:18:29 +0900

    dd

--- Assignment05/Source/Assignment5/Assignment5.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Assignment5.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Assignment5, "Assignment5" );
